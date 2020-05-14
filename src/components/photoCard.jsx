948b44816f4912b4104aae1217f3dec14586e07c
zaldanaraul <raularmandozc12@gmail.com>
2020-05-14 18:52:31 -0400

    added smaller thumbnails to photography site

--- src/components/photoCard.jsx
+++ src/components/photoCard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 const PhotoCard = (props) => {
   let photo = props.photo;
-  let url = `https://zaldanaraul.s3.ca-central-1.amazonaws.com/photos/${photo}.jpg`;
+  let url = `https://zaldanaraul.s3.ca-central-1.amazonaws.com/thumbnails/${photo}.jpg`;
   return (
     <div className="image">
       <img src={url} />
