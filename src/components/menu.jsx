99eb34f95cbe859a7f5c340c2f93720786a916ed
zaldanaraul <raularmandozc12@gmail.com>
2020-05-17 14:57:39 -0400

    added transitions between views

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Brand from "./brand";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faBars,
+  faMapMarkerAlt,
+  faGraduationCap,
+  faLink,
+} from "@fortawesome/free-solid-svg-icons";
 
 /* This is the side-panel on the left in both the desktop and
 mobile versions. In the mobile version, the panel is hidden
@@ -8,55 +21,91 @@ by default and slides into view when pressing the button on
 the Navbar */
 const Menu = (props) => {
   return (
-    <ul className="nav flex-column">
-      <li>
-        <Brand />
-      </li>
+    <div className="container p-0">
+      <div className="row">
+        <div className="col-12 pr-0">
+          <ul className="nav flex-column">
+            <li>
+              <Brand />
+            </li>
 
-      <li className="nav-item">
-        <NavLink
-          to="/aboutme"
-          onClick={props.closeMenu}
-          className="nav-link"
-          activeClassName="active"
-        >
-          About Me
-        </NavLink>
-      </li>
+            <li className="nav-item">
+              <NavLink
+                to="/aboutme"
+                onClick={props.closeMenu}
+                className="nav-link"
+                activeClassName="active"
+              >
+                About Me
+              </NavLink>
+            </li>
 
-      <li className="nav-item">
-        <NavLink
-          to="/projects"
-          className="nav-link"
-          activeClassName="active"
-          onClick={props.closeMenu}
-        >
-          Projects
-        </NavLink>
-      </li>
+            <li className="nav-item">
+              <NavLink
+                to="/projects"
+                className="nav-link"
+                activeClassName="active"
+                onClick={props.closeMenu}
+              >
+                Projects
+              </NavLink>
+            </li>
 
-      <li className="nav-item">
-        <NavLink
-          to="/photography"
-          className="nav-link"
-          activeClassName="active"
-          onClick={props.closeMenu}
-        >
-          Photography
-        </NavLink>
-      </li>
+            <li className="nav-item">
+              <NavLink
+                to="/photography"
+                className="nav-link"
+                activeClassName="active"
+                onClick={props.closeMenu}
+              >
+                Photography
+              </NavLink>
+            </li>
 
-      <li className="nav-item">
-        <NavLink
-          to="/generative-design"
-          className="nav-link"
-          activeClassName="active"
-          onClick={props.closeMenu}
+            <li className="nav-item">
+              <NavLink
+                to="/generative-design"
+                className="nav-link"
+                activeClassName="active"
+                onClick={props.closeMenu}
+              >
+                Generative Design
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="row pl-3 pr-3 mt-5 mb-5">
+        <a
+          className="col-12 mt-2 mb-2 social-media"
+          href="https://www.linkedin.com/in/raulzaldanacalles/"
+          target="_blank"
         >
-          Generative Design
-        </NavLink>
-      </li>
-    </ul>
+          <FontAwesomeIcon style={{ fontSize: "1.5em" }} icon={faLinkedin} />
+        </a>
+        <a
+          className="col-12 mt-2 mb-2 social-media"
+          href="https://github.com/zaldanaraul"
+          target="_blank"
+        >
+          <FontAwesomeIcon style={{ fontSize: "1.5em" }} icon={faGithub} />
+        </a>
+        <a
+          className="col-12 mt-2 mb-2 social-media"
+          href="https://twitter.com/RaulZaldana"
+          target="_blank"
+        >
+          <FontAwesomeIcon style={{ fontSize: "1.5em" }} icon={faTwitter} />
+        </a>
+        <a
+          className="col-12 mt-2 mb-2 social-media"
+          href="https://www.instagram.com/ralzal/"
+          target="_blank"
+        >
+          <FontAwesomeIcon style={{ fontSize: "1.5em" }} icon={faInstagram} />
+        </a>
+      </div>
+    </div>
   );
 };
 
--- src/views/aboutMe.jsx
+++ src/views/aboutMe.jsx
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
+import s3Logo from "../logos/aws-s3.svg";
+import p5Logo from "../logos/p5js.svg";
+import lambdaLogo from "../logos/aws-lambda.png";
+import reactLogo from "../logos/React.webp";
+import rdsLogo from "../logos/rds.webp";
+
+import "../aboutMe.css";
 
 const AboutMe = () => {
   return (
     <React.Fragment>
-      <h1>This is the about me page</h1>
-      <p>This is some info on me</p>{" "}
+      <div className="container main">
+        <div className="row">
+          <div className="col-md-5 col-12">
+            <img
+              src="https://zaldanaraul.s3.ca-central-1.amazonaws.com/photos/profile.jpg"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-md-7 col-12">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <h1>Hi, I'm Raul</h1>
+                  <p>
+                    I'm a Web Developer and AWS Certified Solutions Architect
+                    based in Toronto, Canada. My interests range from Cloud
+                    Technologies and Systems Architecture, on the infrastructure
+                    side of things, to Generative Design and Photography, on the
+                    interaction side.
+                  </p>
+
+                  <p>
+                    Here are some of the technologies I've been using recently:
+                  </p>
+                  {/* <ul>
+                    <li>React.js</li>
+                    <li>P5.js</li>
+                    <li>Javascript (ES6+)</li>
+                    <li>AWS Lambda</li>
+                    <li>Amazon RDS</li>
+                    <li>Amazon CloudFront</li>
+                    <li>MySQL</li>
+                  </ul>*/}
+
+                  <img src={s3Logo} className="tech-logo img-fluid" />
+                  <img src={p5Logo} className="tech-logo img-fluid" />
+                  <img src={lambdaLogo} className="tech-logo img-fluid" />
+                  <img src={reactLogo} className="tech-logo img-fluid" />
+                  <img src={rdsLogo} className="tech-logo img-fluid" />
+                </div>
+              </div>
+              <div className="row"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
