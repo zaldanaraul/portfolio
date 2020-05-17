e0b410a5da08827772020c65b4a43b4a4b2f4b7a
zaldanaraul <raularmandozc12@gmail.com>
2020-05-17 18:54:28 -0400

    done with aboutme layout

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
             </div>
 
             {/*This is the div that contains the content*/}
-            <div className="col-md-10 col-12">
+            <div className="col-md-10 col-12 content-div">
               <Switch>
                 <Route exact path="/">
                   <Photography />
--- src/views/aboutMe.jsx
+++ src/views/aboutMe.jsx
@@ -10,45 +10,73 @@ import "../aboutMe.css";
 const AboutMe = () => {
   return (
     <React.Fragment>
-      <div className="container main">
-        <div className="row">
+      <div className="container main h-100">
+        <div className="row h-100">
           <div className="col-md-5 col-12">
             <img
               src="https://zaldanaraul.s3.ca-central-1.amazonaws.com/photos/profile.jpg"
-              className="img-fluid"
+              className="img-fluid profile-photo"
             />
           </div>
-          <div className="col-md-7 col-12">
+          <div className="col-md-5 col-12 text-justify">
             <div className="container">
               <div className="row">
-                <div className="col-12">
-                  <h1>Hi, I'm Raul</h1>
+                <div className="col-12 copy">
                   <p>
-                    I'm a Web Developer and AWS Certified Solutions Architect
-                    based in Toronto, Canada. My interests range from Cloud
-                    Technologies and Systems Architecture, on the infrastructure
-                    side of things, to Generative Design and Photography, on the
-                    interaction side.
+                    I'm a <mark>Web Developer</mark> and{" "}
+                    <mark>AWS Certified Solutions Architect</mark> based in
+                    Toronto, Canada.{" "}
                   </p>
 
                   <p>
-                    Here are some of the technologies I've been using recently:
+                    My interests range from Cloud Technologies and Systems
+                    Architecture, on the infrastructure side of things, to
+                    Generative Design and Photography, on the interaction side.
                   </p>
+
+                  <p>
+                    Here are some of the technologies I've been using for recent
+                    projects:
+                  </p>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-6">
+                        <p>React.js</p>
+                      </div>
+                      <div className="col-6">
+                        <p>P5.js</p>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-6">
+                        <p>MySQL</p>
+                      </div>
+                      <div className="col-6">
+                        <p>AWS Lambda</p>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-6">
+                        <p>Amazon CloudFront</p>
+                      </div>
+                      <div className="col-6">
+                        <p>Amazon S3</p>
+                      </div>
+                    </div>
+                  </div>
                   {/* <ul>
                     <li>React.js</li>
                     <li>P5.js</li>
                     <li>Javascript (ES6+)</li>
-                    <li>AWS Lambda</li>
-                    <li>Amazon RDS</li>
-                    <li>Amazon CloudFront</li>
+                    
                     <li>MySQL</li>
                   </ul>*/}
 
-                  <img src={s3Logo} className="tech-logo img-fluid" />
+                  {/* <img src={s3Logo} className="tech-logo img-fluid" />
                   <img src={p5Logo} className="tech-logo img-fluid" />
                   <img src={lambdaLogo} className="tech-logo img-fluid" />
                   <img src={reactLogo} className="tech-logo img-fluid" />
-                  <img src={rdsLogo} className="tech-logo img-fluid" />
+                  <img src={rdsLogo} className="tech-logo img-fluid" />*/}
                 </div>
               </div>
               <div className="row"></div>
