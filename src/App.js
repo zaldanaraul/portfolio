8c9438934bda0c8b69f4e4621603f1a83dc0f9f0
zaldanaraul <raularmandozc12@gmail.com>
2020-05-17 17:12:28 -0400

    removed transitions

--- src/App.js
+++ src/App.js
@@ -12,16 +12,8 @@ import {
 import AboutMe from "./views/aboutMe";
 import Projects from "./views/projects";
 import GenerativeDesign from "./views/generativeDesign";
-import { useTransition, animated } from "react-spring";
 
 function App() {
-  const location = useLocation();
-  const transitions = useTransition(location, (location) => location.pathname, {
-    from: { position: "absolute", opacity: 0 },
-    enter: { position: "absolute", opacity: 1 },
-    leave: { position: "absolute", opacity: 0 },
-  });
-
   return (
     <div>
       <div className="d-block d-md-none">
@@ -42,34 +34,30 @@ function App() {
 
             {/*This is the div that contains the content*/}
             <div className="col-md-10 col-12">
-              {transitions.map(({ item, props, key }) => (
-                <animated.div key={key} style={props}>
-                  <Switch location={item}>
-                    <Route exact path="/">
-                      <Photography />
-                    </Route>
-                    {/* Route to photography view */}
-                    <Route path="/photography">
-                      <Photography />
-                    </Route>
+              <Switch>
+                <Route exact path="/">
+                  <Photography />
+                </Route>
+                {/* Route to photography view */}
+                <Route path="/photography">
+                  <Photography />
+                </Route>
 
-                    {/* Route to AboutMe view */}
-                    <Route path="/aboutme">
-                      <AboutMe />
-                    </Route>
+                {/* Route to AboutMe view */}
+                <Route path="/aboutme">
+                  <AboutMe />
+                </Route>
 
-                    {/* Route to Projects view */}
-                    <Route path="/projects">
-                      <Projects />
-                    </Route>
+                {/* Route to Projects view */}
+                <Route path="/projects">
+                  <Projects />
+                </Route>
 
-                    {/* Route to Generative Design view */}
-                    <Route path="/generative-design">
-                      <GenerativeDesign />
-                    </Route>
-                  </Switch>
-                </animated.div>
-              ))}
+                {/* Route to Generative Design view */}
+                <Route path="/generative-design">
+                  <GenerativeDesign />
+                </Route>
+              </Switch>
             </div>
             {/* This is where the content ends */}
           </div>
